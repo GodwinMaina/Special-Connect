9296502d3b4e76e938c4a3aef09b5f1b996f1ac9
GodwinMaina <goddymaina6@gmail.com>
2024-03-14 23:42:19 +0300

    designing landing page

--- frontend/specialConnect/src/app/components/landing-page/landing-page.component.ts
+++ frontend/specialConnect/src/app/components/landing-page/landing-page.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+
+  constructor(){}
 
 }
--- frontend/specialConnect/src/app/interface/postJobs.ts
+++ frontend/specialConnect/src/app/interface/postJobs.ts
@@ -14,6 +14,7 @@ export interface postJobInterface {
 export  interface alljobs {
 
   message: [{
+    job_id: string;
     jobName: string;
     category: string;
     description: string;
@@ -35,6 +36,7 @@ export  interface alljobs {
 export  interface jobCategory {
 
   message: [{
+    job_id: string;
     jobName: string;
     category: string;
     description: string;
